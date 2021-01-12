c2483469661256525aff02a72336d3a0c41deacb
Daniel Martins <martinsda@gmail.com>
2021-01-11 23:40:04 -0300

    Added first route

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log('first commit');
+import Express from 'express';
+
+const app = Express();
+
+app.get('/', (req, res) => {
+    res.send('Hello, 2021!');
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+})
+
